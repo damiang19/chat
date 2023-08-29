18a74f8f902323cda3498116b27742184f5e95cd
Damian <damiang19@gmail.com>
2023-08-29 20:46:18 +0200

    Update configuration - exclude DataSourceAutoConfiguration.class

--- src/main/java/com/dagoreca/chat/SuperChatApplication.java
+++ src/main/java/com/dagoreca/chat/SuperChatApplication.java
@@ -2,8 +2,9 @@ package com.dagoreca.chat;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class SuperChatApplication {
 
 	public static void main(String[] args) {
